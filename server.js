4b9efa7589f3dd78021a2e5c5414b0319e6b8ea5
soumya1805 <149503101+soumya1805@users.noreply.github.com>
2024-02-10 13:32:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
+const express = require ("require");
+const path = require("path");
 
+const app = express();
+const server = require('http').createServer(app);
+
+app.use(express.static(path.join(__dirname+"/public")));
+
+server.listen(5000);
